488faed9a3b17106e48627b8fc3e4864545e58b5
maniamartial <martialmania19@gmail.com>
2022-08-09 21:19:29 +0300

    Authentication complete

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,36 +1,78 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
-export default function Register() {
+function Register() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    password1: "",
+    password: "",
     password2: "",
   });
 
-  const { name, email, password1, password2 } = formData;
+  const { name, email, password, password2 } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error("Thats a bullshit", message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-      [e.target.email]: e.target.email,
-      [e.target.password1]: e.target.password1,
-      [e.target.password2]: e.target.password2,
     }));
   };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      toast.error("Passwords do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
+    }
+  };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
-      <section>
+      <section className="heading">
         <h1>
-          <FaUser />
-          Register
+          <FaUser /> Register
         </h1>
         <p>Please create an account</p>
       </section>
 
       <section className="form">
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="form-group">
             <input
               type="text"
@@ -42,7 +84,6 @@ export default function Register() {
               onChange={onChange}
             />
           </div>
-
           <div className="form-group">
             <input
               type="email"
@@ -54,36 +95,37 @@ export default function Register() {
               onChange={onChange}
             />
           </div>
-
           <div className="form-group">
             <input
               type="password"
               className="form-control"
-              id="password1"
-              name="password1"
-              value={password1}
-              placeholder="Enter your password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter password"
               onChange={onChange}
             />
           </div>
-
           <div className="form-group">
             <input
               type="password"
               className="form-control"
               id="password2"
-              name="name"
+              name="password2"
               value={password2}
               placeholder="Confirm password"
               onChange={onChange}
             />
           </div>
-
           <div className="form-group">
-            <button className="btn btn-block">Submit</button>
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
           </div>
         </form>
       </section>
     </>
   );
 }
+
+export default Register;
