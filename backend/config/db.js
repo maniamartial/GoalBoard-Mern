80ae42db5811155aaa23bb25b4f2b90e3310e285
maniamartial <martialmania19@gmail.com>
2022-08-10 23:29:35 +0300

    Comments on model

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
 
+//Function to connect mongodb with our site
 const connectDB = async () => {
   try {
+    //connect by importing the password and username hidden from the env
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    //print the comment if the site is connected to externala mongodb
     console.log(
       `MongoDB Connected successfully: ${conn.connection.host}`.cyan.underline
     );
   } catch (error) {
     console.log(error);
+    ///if you fall into an error just terminate the process
     process.exit(1);
   }
 };
+
+//make sure you export for it to be used outside the file
 module.exports = connectDB;
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
+//Schema is a single table
 const goalSchema = mongoose.Schema(
   {
+    //user is used to indicate the rlationship 1-1
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
     },
+
+    //This is an entity or a field, its called a text which will only allow string,
     text: {
       type: String,
       required: [true, "Please add a text value mf"],
@@ -17,4 +21,16 @@ const goalSchema = mongoose.Schema(
   }
 );
 
+const lostSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [false, "Try to enter lol"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+//Export your function to be used n an external files
 module.exports = mongoose.model("Goal", goalSchema);
