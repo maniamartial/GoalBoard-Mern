a80860b82ab542d5a801e7087bea93484fd4dbb6
maniamartial <martialmania19@gmail.com>
2022-08-12 19:15:03 +0300

    Sending data to the backend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Spinner from "../components/Spinner";
+import GoalForm from "../components/GoalForm";
 import React from "react";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  //check if the usr is loggedIn, if not return back to login page
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [navigate, user]);
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+    </>
+  );
 }
