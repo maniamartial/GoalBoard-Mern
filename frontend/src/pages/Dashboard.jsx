b12f2879bd82e36110f87c329f084c5a7f7e5237
maniamartial <martialmania19@gmail.com>
2022-08-13 16:46:36 +0300

    Display goals

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,20 +3,35 @@ import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import Spinner from "../components/Spinner";
 import GoalForm from "../components/GoalForm";
+import GoalItem from "../components/GoalItem";
 import React from "react";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 export default function Dashboard() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
   //check if the usr is loggedIn, if not return back to login page
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
     if (!user) {
       navigate("/login");
     }
-  }, [navigate, user]);
+    dispatch(getGoals);
+    return () => {
+      dispatch(reset());
+    };
+  }, [navigate, user, isError, dispatch, message]);
 
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <>
       <section className="heading">
@@ -25,6 +40,17 @@ export default function Dashboard() {
       </section>
 
       <GoalForm />
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>No goals available lol</h3>
+        )}
+      </section>
     </>
   );
 }
